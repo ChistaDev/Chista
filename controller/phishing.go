aea07f63a0b07b302751e4520708885dfcbd8f90
rbozburun <resul.bozburun00@gmail.com>
2023-08-06 15:15:54 +0300

    Models folder created

--- controller/phishing.go
+++ controller/phishing.go
@@ -1,8 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Chista-Framework/Chista/logger"
+	"github.com/gin-gonic/gin"
+)
 
 // GET /api/v1/phishing - List all of the latest phishing domains that related with the supplied query param
 func GetPhishingDomains(ctx *gin.Context) {
+	// Bind URL query parameters to a model
+	logger.Log.Debugln("GetPhishingDomains endpoint called.")
+	//query_phishing_domain_model := models.User{}
 
 }
