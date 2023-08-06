6ef4548e80fbe7bb6e1fe2021970eb002f4ca6e5
rbozburun <resul.bozburun00@gmail.com>
2023-08-06 16:43:13 +0300

    Get the phishing domain from user

--- controller/phishing.go
+++ controller/phishing.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/Chista-Framework/Chista/logger"
+	"github.com/Chista-Framework/Chista/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +10,14 @@ import (
 func GetPhishingDomains(ctx *gin.Context) {
 	// Bind URL query parameters to a model
 	logger.Log.Debugln("GetPhishingDomains endpoint called.")
-	//query_phishing_domain_model := models.User{}
+
+	// Set the queryied model, should bind.
+	query_phishing_domain_model := models.PhishingDomain{}
+	query_phishing_domain_model.Domain = ctx.Query("domain")
+
+	// Set the model for crawled domain list
+	possible_phishing_domains := []models.PhishingDomain{}
+
+	// Apply leveinsthein algortihm to generate new domains
 
 }
