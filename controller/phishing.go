05f5028f197665fefac9e4820393fdb39fa58467
rbozburun <resul.bozburun00@gmail.com>
2023-10-08 02:16:30 +0300

    OpenSquat implemented

--- controller/phishing.go
+++ controller/phishing.go
@@ -1,9 +1,12 @@
 package controller
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/Chista-Framework/Chista/helpers"
 	"github.com/Chista-Framework/Chista/logger"
@@ -11,7 +14,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var LevenshteinDomains_Registered []models.ResponseDomain
+var (
+	LevenshteinDomains_Registered []models.ResponseDomain
+	ORIGINAL_WORKING_DIR          string
+)
 
 // TO DO
 // [] Handle domains shorter than 3 letter hostnames like 'ab.com', 'au.com'
@@ -19,7 +25,7 @@ var LevenshteinDomains_Registered []models.ResponseDomain
 // GET /api/v1/phishing - List all of the latest phishing domains that related with the supplied query param
 func GetPhishingDomains(ctx *gin.Context) {
 	// [x] Check dnstwister.it
-	// [] Check opensquat
+	// [x] Check opensquat
 	// [] Check search.censys.io for SSL cert transparency
 	// Check HTTP/S services for detected domains, if the HTTP/s running, report them as phishing.
 
@@ -38,8 +44,9 @@ func GetPhishingDomains(ctx *gin.Context) {
 
 		// Check dnstwister.it
 		// Request to get hex form of domain -> https://dnstwister.report/api/to_hex/{domain}
-		logger.Log.Debugf("Domain: %v", query_phishing_domain_model.Domain)
+		logger.Log.Infof("Queried domain: %v", query_phishing_domain_model.Domain)
 		dnstwister_toHex_url := "http://dnstwister.report/api/to_hex/" + query_phishing_domain_model.Domain
+		logger.Log.Infoln("Sending request to dnstwister for hex conversion of domain...")
 		response, err := http.Get(dnstwister_toHex_url)
 		if err != nil {
 			logger.Log.Errorln("Error decoding response body:", err)
@@ -57,6 +64,7 @@ func GetPhishingDomains(ctx *gin.Context) {
 
 		// Fuzz the other domains -> https://dnstwister.report/api/fuzz/{domain_hex}
 		dnstwister_fuzz_url := dnstwister_toHex_response.FuzzURL
+		logger.Log.Infoln("Fuzzing the possible phishing domains from dnstwister...")
 		response, err = http.Get(dnstwister_fuzz_url)
 		if err != nil {
 			logger.Log.Errorln("Error decoding response body:", err)
@@ -73,17 +81,99 @@ func GetPhishingDomains(ctx *gin.Context) {
 		decoder = json.NewDecoder(response.Body)
 		if err := decoder.Decode(&dnstwister_fuzzResponse); err != nil {
 			logger.Log.Errorln("Error decoding response body - dnstwister fuzz:", err)
+			msg := "Error decoding response body - dnstwister fuzz:" + err.Error()
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
+		logger.Log.Infoln("Domains fuzzed! [DnsTwister]")
 
-		//ctx.JSON(http.StatusOK, &dnstwister_fuzzResponse)
+		//logger.Log.Infof("Fuzzed domains: %v", dnstwister_fuzzResponse)
 
 		// Check opensquat
 		// python37.exe opensquat.py  --phishing ph_results.txt
 		// 		Reads the keywords.txt for keywords, and searches phishing domains
 		//		Possible phishing results -> results.txt
+		// Command and arguments to run the Python script
+		python_executable_path := helpers.GoDotEnvVariable("PY_PATH")
+		logger.Log.Infof("Using Python executable: %v", python_executable_path)
+		opensquat_py_path := helpers.GoDotEnvVariable("OPENSQUAT_PY_PATH")
+		logger.Log.Infof("Using OpenSquat python file: %v/opensquat.py", opensquat_py_path)
 
-		return
+		// Store the current working directory
+		ORIGINAL_WORKING_DIR, err = os.Getwd()
+		if err != nil {
+			logger.Log.Errorf("Error getting current working directory: %v", err)
+			return
+		}
+
+		err = os.Chdir(opensquat_py_path)
+		if err != nil {
+			logger.Log.Errorf("Cannot change working directory for API worker - OpenSquat.py: %v", err)
+			msg := "Cannot change working directory for API worker - OpenSquat.py:" + err.Error()
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		// Set the keywords.txt to include queried domain
+		file, err := os.Create("keywords.txt")
+		if err != nil {
+			logger.Log.Errorf("Cannot create keywords.txt  for API worker - OpenSquat.py: %v", err)
+		}
+		defer file.Close() // Ensure the file is closed when done.
+
+		// Create a buffered writer to efficiently write to the file
+		writer := bufio.NewWriter(file)
+
+		// Write keyword to the file
+		keyword := query_phishing_domain_model.Hostname
+		_, err = writer.WriteString(keyword)
+		if err != nil {
+			logger.Log.Errorf("Error writing to file - OpenSquat.py: %v", err)
+		}
+
+		// Flush the buffered writer to ensure data is written to the file
+		writer.Flush()
+
+		cmd := exec.Command(python_executable_path, "opensquat.py", "--phishing", "ph_results.txt")
+		_, err = cmd.CombinedOutput()
+		if err != nil {
+			logger.Log.Errorln("Error while executing the OpenSquat command:", err)
+		}
+		//logger.Log.Debugf("OpenSquat command executed: %v", string(output))
+
+		// Open the file for reading
+		file, err = os.Open("results.txt")
+		if err != nil {
+			logger.Log.Errorf("Error opening results.txt file - OpenSquat: %v", err)
+		}
+		defer file.Close() // Ensure the file is closed when done.
+
+		// Create an array to hold the lines
+		var opensquat_phishing_domains []string
+
+		// Create a scanner to read lines from the file
+		scanner := bufio.NewScanner(file)
+
+		// Read each line from the file
+		for scanner.Scan() {
+			line := scanner.Text()
+			opensquat_phishing_domains = append(opensquat_phishing_domains, line)
+		}
+
+		// Check for any scanner errors
+		if err := scanner.Err(); err != nil {
+			logger.Log.Errorf("Error reading the results.txt file - OpenSquat: %v", err)
+			return
+		}
+
+		//ctx.JSON(http.StatusOK, gin.H{"Phishing domains by OpenSquat": opensquat_phishing_domains})
+
+		// Reset the working directory to its original value
+		err = os.Chdir(ORIGINAL_WORKING_DIR)
+		if err != nil {
+			logger.Log.Errorf("Error resetting directory: %v", err)
+			return
+		}
 
 	} else {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "domain missing"})
